7198af89dc31685b49682f08e0315f5d88271ef1
Maguti19 <143103741+Maguti19@users.noreply.github.com>
2023-10-06 17:23:11 -0500

    Update AplicacionAMA.py

--- AplicacionAMA.py
+++ AplicacionAMA.py
@@ -1,41 +1,30 @@
 import streamlit as st
 import os
 import time
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
 import glob
-import os
 from googletrans import Translator
 from gtts import gTTS
 from PIL import Image
 
-st.title("Aplicativo AMA.")
-#image = Image.open('text_to_audio.png')
-
-#st.image(image, width=200)
-
-
+# Crear directorio temporal si no existe
 try:
     os.mkdir("temp")
-except:
+except FileExistsError:
     pass
 
+st.title("Aplicativo AMA.")
+
 st.subheader("Texto a audio.")
-st.write('Las interfaces de texto a audio son fundamentales en las interfaces multimodales ya que permiten '  
-         'una comunicación más accesible y natural, facilitando la inclusión de personas con discapacidades ' 
-         ' visuales y permitiendo la interacción en situaciones donde no es posible leer texto. Estas interfaces '  
-         ' también impulsan tecnologías emergentes como los asistentes de voz inteligentes, haciendo que la tecnología ' 
-         ' sea más accesible e intuitiva para todos los usuarios')
-           
+st.write("Las interfaces de texto a audio son fundamentales en las interfaces multimodales ya que permiten "
+         "una comunicación más accesible y natural, facilitando la inclusión de personas con discapacidades "
+         "visuales y permitiendo la interacción en situaciones donde no es posible leer texto. Estas interfaces "
+         "también impulsan tecnologías emergentes como los asistentes de voz inteligentes, haciendo que la tecnología "
+         "sea más accesible e intuitiva para todos los usuarios")
 
-text = st.text_input("Ingrese el texto.")
-
-tld="es"
+text = st.text_input("Ingrese el texto:")
 
 def text_to_speech(text, tld):
-    
-    tts = gTTS(text,"es", tld, slow=False)
+    tts = gTTS(text, "es", tld, slow=False)
     try:
         my_file_name = text[0:20]
     except:
@@ -43,20 +32,14 @@ def text_to_speech(text, tld):
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name, text
 
-
-#display_output_text = st.checkbox("Verifica el texto")
-
-if st.button("convertir"):
+if st.button("Convertir"):
     result, output_text = text_to_speech(text, tld)
     audio_file = open(f"temp/{result}.mp3", "rb")
     audio_bytes = audio_file.read()
-    st.markdown(f"## Tú audio:")
+    st.markdown("## Tu audio:")
     st.audio(audio_bytes, format="audio/mp3", start_time=0)
-
-    #if display_output_text:
-    st.markdown(f"## Texto en audio:")
-    st.write(f" {output_text}")
-
+    st.markdown("## Texto en audio:")
+    st.write(f"{output_text}")
 
 def remove_files(n):
     mp3_files = glob.glob("temp/*mp3")
@@ -66,150 +49,20 @@ def remove_files(n):
         for f in mp3_files:
             if os.stat(f).st_mtime < now - n_days:
                 os.remove(f)
-                print("Deleted ", f)
-st.write("Toca el Botón y habla lo que quires traducir")
+                print("Deleted", f)
 
-stt_button = Button(label=" Inicio ", width=200)
+st.write("Toca el botón y habla lo que quieres traducir")
 
-stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
- 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-    """))
+stt_button = st.button("Inicio", key="stt_button")
 
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
+if stt_button:
+    st.write("Habla y espera a que se traduzca automáticamente.")
 
-if result:
-    if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
-    try:
-        os.mkdir("temp")
-    except:
-        pass
-    st.title("Texto a Audio")
-    translator = Translator()
+    result = st.text_area("Texto traducido:")
     
-    text = str(result.get("GET_TEXT"))
-    in_lang = st.selectbox(
-        "Selecciona el lenguaje de Entrada",
-        ("Inglés", "Español", "Italiano", "Coreano", "Francés", "Japonés"),
-    )
-    if in_lang == "Inglés":
-        input_language = "en"
-    elif in_lang == "Español":
-        input_language = "es"
-    elif in_lang == "Italiano":
-        input_language = "it"
-    elif in_lang == "Coreano":
-        input_language = "ko"
-    elif in_lang == "Francés":
-        input_language = "fr"
-    elif in_lang == "Japonés":
-        input_language = "ja"
-    
-    out_lang = st.selectbox(
-        "Selecciona el lenguaje de salida",
-        ("Inglés", "Español", "Italiano", "Coreano", "Francés", "Japonés"),
-    )
-    if out_lang == "Inglés":
-        output_language = "en"
-    elif out_lang == "Español":
-        output_language = "es"
-    elif out_lang == "Italiano":
-        output_language = "it"
-    elif out_lang == "Coreano":
-        output_language = "ko"
-    elif out_lang == "Francés":
-        output_language = "fr"
-    elif out_lang == "Japonés":
-        output_language = "ja"
-    
-    english_accent = st.selectbox(
-        "Selecciona el acento",
-        (
-            "Defecto",
-            "Español",
-            "Reino Unido",
-            "Estados Unidos",
-            "Canada",
-            "Australia",
-            "Irlanda",
-            "Sudáfrica",
-        ),
-    )
-    
-    if english_accent == "Defecto":
-        tld = "com"
-    elif english_accent == "Español":
-        tld = "com.mx"
-    
-    elif english_accent == "Reino Unido":
-        tld = "co.uk"
-    elif english_accent == "Estados Unidos":
-        tld = "com"
-    elif english_accent == "Canada":
-        tld = "ca"
-    elif english_accent == "Australia":
-        tld = "com.au"
-    elif english_accent == "Irlanda":
-        tld = "ie"
-    elif english_accent == "Sudáfrica":
-        tld = "co.za"
-    
-    
-    def text_to_speech(input_language, output_language, text, tld):
-        translation = translator.translate(text, src=input_language, dest=output_language)
-        trans_text = translation.text
-        tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
-        try:
-            my_file_name = text[0:20]
-        except:
-            my_file_name = "audio"
-        tts.save(f"temp/{my_file_name}.mp3")
-        return my_file_name, trans_text
-    
-    
-    display_output_text = st.checkbox("Mostrar el texto")
-    
-    if st.button("convertir"):
-        result, output_text = text_to_speech(input_language, output_language, text, tld)
-        audio_file = open(f"temp/{result}.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.markdown(f"## Tú audio:")
-        st.audio(audio_bytes, format="audio/mp3", start_time=0)
-    
-        if display_output_text:
-            st.markdown(f"## Texto de salida:")
-            st.write(f" {output_text}")
-    
-    
-    def remove_files(n):
-        mp3_files = glob.glob("temp/*mp3")
-        if len(mp3_files) != 0:
-            now = time.time()
-            n_days = n * 86400
-            for f in mp3_files:
-                if os.stat(f).st_mtime < now - n_days:
-                    os.remove(f)
-                    print("Deleted ", f)
+    stt_button = st.button("Detener")
+
+    if stt_button:
+        st.text("Detenido")
 
 remove_files(7)
