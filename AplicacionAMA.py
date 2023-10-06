71d4321b141a97f986c7b1e395ae6b71883d87af
Maguti19 <143103741+Maguti19@users.noreply.github.com>
2023-10-06 17:32:11 -0500

    Update AplicacionAMA.py

--- AplicacionAMA.py
+++ AplicacionAMA.py
@@ -2,67 +2,71 @@ import streamlit as st
 import os
 import time
 import glob
-from googletrans import Translator
 from gtts import gTTS
+from googletrans import Translator
 from PIL import Image
 
-# Crear directorio temporal si no existe
 try:
     os.mkdir("temp")
-except FileExistsError:
+except:
     pass
 
-st.title("Aplicativo AMA.")
+st.title("Aplicativo AMA")
+#image = Image.open('text_to_audio.png')
+#st.image(image, width=200)
 
-st.subheader("Texto a audio.")
-st.write("Las interfaces de texto a audio son fundamentales en las interfaces multimodales ya que permiten "
-         "una comunicación más accesible y natural, facilitando la inclusión de personas con discapacidades "
-         "visuales y permitiendo la interacción en situaciones donde no es posible leer texto. Estas interfaces "
-         "también impulsan tecnologías emergentes como los asistentes de voz inteligentes, haciendo que la tecnología "
-         "sea más accesible e intuitiva para todos los usuarios")
+st.subheader("Texto a audio y traducción.")
 
-text = st.text_input("Ingrese el texto:")
+text = st.text_input("Ingrese el texto que desea traducir y convertir en audio.")
 
-def text_to_speech(text, tld):
-    tts = gTTS(text, "es", tld, slow=False)
+translator = Translator()
+
+if st.button("Traducir y Convertir a Audio"):
+    in_lang = st.selectbox(
+        "Selecciona el lenguaje de Entrada",
+        ("Inglés", "Español", "Italiano", "Coreano", "Francés", "Japonés"),
+    )
+    if in_lang == "Inglés":
+        input_language = "en"
+    elif in_lang == "Español":
+        input_language = "es"
+    # ... (agregar más idiomas según sea necesario)
+
+    out_lang = st.selectbox(
+        "Selecciona el lenguaje de salida",
+        ("Inglés", "Español", "Italiano", "Coreano", "Francés", "Japonés"),
+    )
+    if out_lang == "Inglés":
+        output_language = "en"
+    elif out_lang == "Español":
+        output_language = "es"
+    # ... (agregar más idiomas según sea necesario)
+
+    translation = translator.translate(text, src=input_language, dest=output_language)
+    translated_text = translation.text
+
+    tts = gTTS(translated_text, lang=output_language, slow=False)
     try:
-        my_file_name = text[0:20]
+        my_file_name = translated_text[0:20]
     except:
         my_file_name = "audio"
     tts.save(f"temp/{my_file_name}.mp3")
-    return my_file_name, text
 
-if st.button("Convertir"):
-    result, output_text = text_to_speech(text, tld)
-    audio_file = open(f"temp/{result}.mp3", "rb")
+    audio_file = open(f"temp/{my_file_name}.mp3", "rb")
     audio_bytes = audio_file.read()
-    st.markdown("## Tu audio:")
     st.audio(audio_bytes, format="audio/mp3", start_time=0)
-    st.markdown("## Texto en audio:")
-    st.write(f"{output_text}")
 
-def remove_files(n):
-    mp3_files = glob.glob("temp/*mp3")
-    if len(mp3_files) != 0:
-        now = time.time()
-        n_days = n * 86400
-        for f in mp3_files:
-            if os.stat(f).st_mtime < now - n_days:
-                os.remove(f)
-                print("Deleted", f)
+    st.markdown(f"## Texto Traducido:")
+    st.write(f" {translated_text}")
 
-st.write("Toca el botón y habla lo que quieres traducir")
+    def remove_files(n):
+        mp3_files = glob.glob("temp/*mp3")
+        if len(mp3_files) != 0:
+            now = time.time()
+            n_days = n * 86400
+            for f in mp3_files:
+                if os.stat(f).st_mtime < now - n_days:
+                    os.remove(f)
+                    print("Deleted ", f)
 
-stt_button = st.button("Inicio", key="stt_button")
-
-if stt_button:
-    st.write("Habla y espera a que se traduzca automáticamente.")
-
-    result = st.text_area("Texto traducido:")
-    
-    stt_button = st.button("Detener")
-
-    if stt_button:
-        st.text("Detenido")
-
-remove_files(7)
+    remove_files(7)
